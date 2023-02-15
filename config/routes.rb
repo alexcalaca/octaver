254a21d4959855a1967fa0dbcfdb55d9285adfa3
Alexandre Calaça <alexcalaca@gmail.com>
2023-02-15 16:39:12 -0300

    feat: set default root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'store/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'store#index'  
 end
