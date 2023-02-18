46970313e0dfab9252d77871eaf841395668341f
Alexandre Calaça <alexcalaca@gmail.com>
2023-02-18 15:31:14 -0300

    feat: annotate models and routes

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: instruments
+#
+#  id          :integer          not null, primary key
+#  brand       :string
+#  model       :string
+#  description :text
+#  condition   :string
+#  finish      :string
+#  title       :string
+#  price       :decimal(5, 2)    default(0.0)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  image       :string
+#  user_id     :integer
+#
 class Instrument < ApplicationRecord
   mount_uploader :image, ImageUploader
   serialize :image, JSON # It is used because of sqlite
