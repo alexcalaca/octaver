0d71aa8fb6ae678790c64454f1091c80a40994e2
Alexandre Calaça <alexcalaca@gmail.com>
2023-02-21 23:44:36 -0300

    feat: create line_items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_21_171924) do
+ActiveRecord::Schema.define(version: 2023_02_21_172456) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2023_02_21_171924) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "user_id"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "instrument_id"
+    t.integer "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["instrument_id"], name: "index_line_items_on_instrument_id"
   end
 
   create_table "users", force: :cascade do |t|
