6a5d66c7d65d1492069e562fd3f4ba07d7b442f0
Alexandre Calaça <alexcalaca@gmail.com>
2023-03-03 12:03:38 -0300

    feat: set cart only on create action

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,8 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
   before_action :set_line_item, only: %i[ show edit update destroy ]
+  before_action :set_cart, only: [:create]
 
-  # GET /line_items or /line_items.json
   def index
     @line_items = LineItem.all
   end
